3a177aa509fe0e142d85ae313d694baaad026caf
Marquimark <marquimark1974@gmail.com>
2024-06-08 11:55:53 -0500

    segundo avance con htmsl de menu

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 
 
 const datosImportantes = document.querySelector('#datosImportantes');
-const datoEjecutivo = document.querySelector('.datoEjecutivo');
-// console.log(datoEjecutivo);
 
-datosImportantes.addEventListener('submit',mostrarData);
+datosImportantes.addEventListener('submit',muestraData);
 
-function mostrarData(event){
+function muestraData(event){
+    const datosEjecutivo = '';
     event.preventDefault();
     const dataImport = new FormData(this);
-    navigator.clipboard.writeText(`
-    Nombre : ${dataImport.get('nombreEjecutivo')}
-    Min : ${dataImport.get('minEjecutivo')}
-    `)    
-
-    .then(() => {
-        salidaManual(dataImport.get('nombreEjecutivo'),dataImport.get('minEjecutivo'));
-    })
-    .catch(err => {
-        alert('Error al copiar al portapapeles:', err)
-        console.log( err );
-        // console.error('Error al copiar al portapapeles:', err)
-    })
+    dataImport.get('nombreEjecutivo')
+    const numeroEjecutivo = dataImport.get('minEjecutivo')
+    window.location.href = './content/manual.html';
 }
 
-function salidaManual(nombre, min){
-        window.location.href = './content/manual.html';
-}
+
+// function mostrarData(event){
+//     event.preventDefault();
+//     const dataImport = new FormData(this);
+//     navigator.clipboard.writeText(`
+//     ${dataImport.get('nombreEjecutivo')}
+//     ${dataImport.get('minEjecutivo')}
+//     `)    
+
+//     .then(() => {
+//         window.location.href = './content/manual.html';
+//         // salidaManual(dataImport.get('nombreEjecutivo'),dataImport.get('minEjecutivo'));
+//     })
+//     .catch(err => {
+//         alert('Error al copiar al portapapeles:', err)
+//         console.log( err );
+//         // console.error('Error al copiar al portapapeles:', err)
+//     })
+// }
--- mainManual.js
+++ mainManual.js
@@ -1,16 +1,17 @@
 
-    // import './main.js';
-    // console.log( recibeInformacion );
 
-    // const nombreo = document.querySelector('.datoEjecutivo');
-    // const datosEjecutivos = document.querySelector('#datosImportantes');
+const muestraData = import('./main.js');
 
-    // datosEjecutivos.addEventListener('submit', utilizaData);
+const nombreEjecutivo = document.querySelector('#datoEjecutivo');
+const ingresoNombre = 'MARCO';
 
-    // function utilizaData(event){
-    //     event.preventDefault();
-    //     const forma = new FormData( this );
-    //     console.log( forma.get('nombreEjecutivo'))
-    //     console.log( forma.get('datoEjecutivo'))
-    // }
-    
+muestraData.then(() => {
+        console.log('saludos')
+        // salidaManual(dataImport.get('nombreEjecutivo'),dataImport.get('minEjecutivo'));
+    })
+    .catch(err => {
+        // alert('Error al copiar al portapapeles:', err)
+        console.log( err );
+        // console.error('Error al copiar al portapapeles:', err)
+     })
+
